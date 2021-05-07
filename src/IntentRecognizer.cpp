8c93fd446391eb8138e8d39551eb41b343908331
pheon-git <patrick_benjamin.heck@daimler.com>
2021-05-07 12:30:11 +0100

    IntentRecognizer: Simple Hardcoded implementation to pass identifyIntent Test

--- src/IntentRecognizer.cpp
+++ src/IntentRecognizer.cpp
@@ -1,5 +1,11 @@
 #include "IntentRecognizer.h"
+#include <map>
 
 string IntentRecognizer::identifyIntent(string userInput){
-    return("");
+    map<string, string> intentMapping = {
+                {"What is the weather like today?","Get Weather"},
+                {"What is the weather like in Paris today?","Get Weather City"},
+                {"Tell me an interesting fact.","Get Fact"}
+    };
+    return(intentMapping[userInput]);
 }
