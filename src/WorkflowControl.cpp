b68715be95d2fd6856ed7db97e050bc9fa3ee1f5
pheon-git <patrick_benjamin.heck@daimler.com>
2021-05-09 21:51:45 +0100

    WorkflowCtrl: Created failing TC for calling HMI and Intent

---
+++ src/WorkflowControl.cpp
@@ -0,0 +1,8 @@
+#include "WorkflowControl.h"
+
+
+WorkflowControl::WorkflowControl(I_IntentRecognizer* recognizer, I_UserIO* ui){}
+void WorkflowControl::startWorkflow(){}
+void WorkflowControl::welcomeMessage(){}
+void WorkflowControl:: serveUser(){}
+void WorkflowControl::goodbyMessage(){}
--- test/test_workflow.cpp
+++ test/test_workflow.cpp
@@ -1,6 +1,10 @@
 #include "gtest/gtest.h"
-
+#include "gmock/gmock.h"
+#include "I_IntentRecognizer.h"
+#include "I_UserIO.h"
+#include "WorkflowControl.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
 
@@ -13,7 +17,45 @@ class workflowTest : public ::testing::Test {
 };
 
 
+class mockRecognizer : public I_IntentRecognizer{
+    public:
+        MOCK_METHOD1(identifyIntent, string(string userInput));
+};
 
-TEST_F(workflowTest, blankTC){
-    // Test case still blank
+class mockHMI : public I_UserIO {
+    public:
+        MOCK_METHOD1(promptUserInput, string(string promptMessage));
+        MOCK_METHOD1(writeOutput, void(string messageOutput));
+};
+
+TEST_F(workflowTest, callIntentRecognizer){
+    // Test case to check if the workflow correctly calls the Intent Recognizer module
+    string testInput = "Test Input";
+    // Create Mock Objects
+    mockRecognizer recognizer;
+    mockHMI hmi;
+
+    WorkflowControl controller(&recognizer, &hmi);
+    EXPECT_CALL(recognizer, identifyIntent(testing::_))
+        .Times(testing::AtLeast(1))
+        .WillOnce(testing::Return("Test Intent"));
+    
+    
+    controller.startWorkflow();
+
 }
+
+TEST_F(workflowTest, callHMI){
+    // Test case to check if the HMI module is called properly
+    string testInput = "Test Input";
+    mockRecognizer recognizer;
+    mockHMI hmi;
+
+    WorkflowControl controller(&recognizer, &hmi);
+    EXPECT_CALL(hmi, promptUserInput(testing::_))
+        .Times(testing::AtLeast(1))
+        .WillOnce(testing::Return("Test Intent"));
+    EXPECT_CALL(hmi, writeOutput(testing::_))
+        .Times(2);
+}
+
